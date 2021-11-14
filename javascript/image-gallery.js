f09a6f300a7108c04992971fb3c21c17f66f9b4c
Paul N <paul_nigolian@hotmail.com>
2021-11-14 21:31:47 +0200

    Added image viewing ability

--- javascript/image-gallery.js
+++ javascript/image-gallery.js
@@ -2,6 +2,7 @@
     var allplants;
     var Index=0;
     window.onload= function(){
+
         console.log("hey");
         loadpage();
         window.onscroll = function()
@@ -16,8 +17,18 @@
              if (difference - scrollposition <= 2)
              {
              
-                    alert("Bottom of Page!"); 
-                
+                fetch('../json/plants2.json')
+                .then(function (response) {
+                    console.log(response)
+                    return response.json();
+                })
+                .then(function (json) {
+                    allplants = JSON.parse(json);
+                    console.log(allplants);
+                    for(var i=0;i<allplants.length;i++){
+                        document.getElementById("image_container").appendChild(createelement(allplants[i])) 
+                    }
+                });
              
              }
             
@@ -36,7 +47,7 @@
         .then(function (json) {
             allplants = JSON.parse(json);
             console.log(allplants);
-            for(var i=0;i<20;i++){
+            for(var i=0;i<12;i++){
                 document.getElementById("image_container").appendChild(createelement(allplants[i])) 
             }
         });
@@ -48,6 +59,8 @@
 function createelement(json){
     var key=json.Images[0].Key;
     var mycontainer=document.createElement("div");
+    mycontainer.object=json;
+    mycontainer.imageIdex=0;
     mycontainer.setAttribute("class","polaroid");
     myimage=document.createElement("div");
     myimage.style.backgroundImage="url(https://landscapeplants.aub.edu.lb"+key+")";
@@ -61,6 +74,45 @@ function createelement(json){
     mycontainer.appendChild(myimage);
     mycontainer.appendChild(Scientificname);
     mycontainer.appendChild(name);
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    mycontainer.onclick = function(){
+    modal.style.display = "block";
+    modal.object=json;
+    modal.index=0;
+    modalImg.src = "https://landscapeplants.aub.edu.lb/"+this.object.Images[0].Key;
+    captionText.innerHTML = this.object.CommonEnglishName;
+    }
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    var right = document.getElementById("right");
+    right.onclick = function() { 
+    modalImg=document.getElementById("img01");
+    modal=document.getElementById("myModal");
+    if (modal.index<modal.object.Images.length-1){
+        modal.index++;
+        modalImg.src= "https://landscapeplants.aub.edu.lb/"+modal.object.Images[modal.index].Key;
+        }
+    }
+
+    var left = document.getElementById("left");
+    left.onclick = function() { 
+        modalImg=document.getElementById("img01");
+        modal=document.getElementById("myModal");
+        if (modal.index>0){
+            modal.index--;
+            modalImg.src= "https://landscapeplants.aub.edu.lb/"+modal.object.Images[modal.index].Key;
+            }
+        }
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
     return mycontainer;
 }
 
@@ -75,3 +127,4 @@ function createelement(json){
 // myimage.style.backgroundImage="url(https://landscapeplants.aub.edu.lb"+key+")";
 // myimage.setAttribute("class", "item_image");
 // document.getElementById("image_container").appendChild(myimage);
+
