aa5d56d5b1521bef1a96110831c130c1b074247e
Paul N <paul_nigolian@hotmail.com>
2021-11-22 12:25:02 +0200

    pauls commit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,12 @@ const port = 3000;
 let url = require('url');
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+//added by paul
+const path = require('path');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/image_gallery.html'));
+})
+/////////////////
 //---------------connecting to MongoDB------------//
 var MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:root@cluster0.usof1.mongodb.net/landscapeAUB?retryWrites=true&w=majority";
@@ -12,6 +17,7 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(client => {
 
         let countriesCollection = client.db("landscapeAUB").collection("countries");
+        let palntsCollection = client.db("landscapeAUB").collection("plants");
         console.log(countriesCollection);
         //---------------------routes------------------------//
 
@@ -24,9 +30,16 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
                 })
                 .catch(error => console.error(error))
         })
-
-        //
-
+        //added by paul
+        app.get("/plants", (req, res) => {
+            console.log(">>>>>>>>>>>>>>>>>>in plants");
+            palntsCollection.find().toArray()
+                .then(results => {
+                    res.send(results)
+                })
+                .catch(error => console.error(error))
+        })
+        /////////////
         app.listen(port, () => {
             console.log(`listening at http://localhost:${port}`)
         })
