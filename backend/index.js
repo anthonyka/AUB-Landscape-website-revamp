75afa37590ca40c9a5518a12d19734fa7d08670b
jadz200 <jadzarzour3@gmail.com>
2021-12-06 16:53:25 +0200

    removes the error

--- backend/index.js
+++ backend/index.js
@@ -778,21 +778,10 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
         })
         app.get("/names", (req, res) => {
-            console.log(req.query);
             let Scientific_Name = req.query.Scientific_Name;
-            /*f(Scientific_Name!=""){
-                Scientific_Name=Scientific_Name.trim();
-                Scientific_Name=Scientific_Name.replaceAll(","," ");
-                Scientific_Name=Scientific_Name.replaceAll("\'","");
-                Scientific_Name=Scientific_Name.replaceAll(/\s+/g," ");
-            }*/
+
             let Common_Name = req.query.Common_Name;
-            /*if(Common_Name!=""){
-                Common_Name=Common_Name.trim();
-                Common_Name=Common_Name.replaceAll(","," ");
-                Common_Name=Common_Name.replaceAll("\'"," ");
-                Common_Name=Common_Name.replaceAll(/\s+/g," ");
-            }*/
+
             let queryOr = { $or: [] };
             if (Scientific_Name!=""){
                 queryOr.$or.push({ScientificName: {$regex:"^"+Scientific_Name,$options:'i'}});
@@ -804,18 +793,8 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
                 queryOr.$or.push({CommonEnglishName: ""});
             }
             
-            allPlantsCollection.find().forEach(function(x){
-                EnglishMongo=x.CommonEnglishName;
-                EnglishMongo=(EnglishMongo.toString()).replaceAll(","," ");
-                EnglishMongo=EnglishMongo.replaceAll("\'","");
-                EnglishMongo=EnglishMongo.replaceAll(/\s+/g," ");
-                //console.log(EnglishMongo);
-            })
-
             allPlantsCollection.find(queryOr).toArray()
             .then(results => {
-                //console.log(results);
-                console.log(queryOr.$or.ScientificName);
                 return res.render("searchResults", { plants: results, partial: -1, filters:{ScientificName: Scientific_Name, CommonName: Common_Name} });
             });
         
